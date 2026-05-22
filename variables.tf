3411c4a14c60f44856dd98dc801ea1b83eac42f6
rahul paladugu <rahulpaladugu96@gmail.com>
2026-05-22 12:40:15 +1200

    terraform-module-aws-ec2 development

--- variables.tf
+++ variables.tf
@@ -28,28 +28,3 @@ variable "ec2_tags" {
   default = {}
 }
 
-variable "zone_id" {
-  type = string
-  description = "zone_id of the project"
-}
-
-variable "r53_project_name" {
-  type = string
-  description = "Please mention your project name"
-}
-
-variable "public_r53_name" {
-  type = string
-  description = "name we configure to access the application"
-}
-
-variable "sg_tags" {
-  type = map
-  description = "tags to add to security group"
-  default = {}
-}
-
-variable "sg_ports" {
-  type = list
-  description = "Ports that needs to be opened"
-}
